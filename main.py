aebee795f122608065bbce1c56ecff616fac42a7
CodeSandbox <support@codesandbox.io>
2023-10-28 07:24:51 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    distance = float(request.form['distance'])
+    fuel_efficiency = float(request.form['fuel_efficiency'])
+    price_per_liter = float(request.form['price_per_liter'])
 
+    fuel_consumed = distance / fuel_efficiency
+    cost = fuel_consumed * price_per_liter
 
-if __name__ == "__main__":
-    app.run()
+    return render_template('index.html', result=fuel_consumed, cost=cost)
+
+if __name__ == '__main__':
+    app.run(debug=True)
